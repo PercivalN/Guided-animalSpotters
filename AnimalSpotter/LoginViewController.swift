12cfe382835474fc0af4a14b6a04ccda4dff4195
Percy <percival.ngan@gmail.com>
2019-09-04 11:06:17 -0500

    Initial commit

---
+++ AnimalSpotter/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  AnimalSpotter
+//
+//  Created by Ben Gohlke on 4/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+enum LoginType {
+    case signUp
+    case signIn
+}
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet private weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Customize button appearance
+        signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+        signInButton.tintColor = .white
+        signInButton.layer.cornerRadius = 8.0
+    }
+    
+    // MARK: - Action Handlers
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        
+    }
+}
+
